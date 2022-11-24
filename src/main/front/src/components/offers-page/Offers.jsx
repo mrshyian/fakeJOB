d1d31a8085d92a0b6c829066f4adcae43f1b2b29
JakubGem <jakegemb89@gmail.com>
2022-11-24 16:25:43 +0100

    offer card: created new sections with css

--- src/main/front/src/components/offers-page/Offers.jsx
+++ src/main/front/src/components/offers-page/Offers.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Card} from "react-bootstrap";
 import img from "../../pictures/img.png";
+import logo from "../../pictures/logoAskme2.png";
+
 import "./offers.css";
+import {GrLocation} from "react-icons/gr";
+import {GiGraduateCap} from "react-icons/gi";
 const Offers = () => {
     return (
         <div>
@@ -53,27 +57,117 @@ const Offers = () => {
             {/*</Card>*/}
 
 
+            {/*<Card className="nested">*/}
+
+            {/*    <div>*/}
+            {/*        <img style={{maxWidth: "200px"}}  src={img} alt=""/>*/}
+            {/*    </div>*/}
+
+            {/*    <div>*/}
+
+            {/*        <div className="innerDiv">*/}
+            {/*            <h2>Oferta praca java fullstack bohater</h2>*/}
+
+            {/*            <select>*/}
+            {/*                <option>3 Localizacji </option>*/}
+            {/*                <option value="1">Poznań</option>*/}
+            {/*                <option value="2">Warszawa</option>*/}
+            {/*                <option value="3">Wrocław</option>*/}
+            {/*            </select>*/}
+
+            {/*            <h4>Wymagania:</h4>*/}
+            {/*            javajavajavajava*/}
+            {/*        </div>*/}
+
+            {/*    </div>*/}
+
+            {/*</Card>*/}
+
             <Card className="nested">
 
                 <div>
-                    <img style={{maxWidth: "200px"}}  src={img} alt=""/>
+                    <img className="com-logo"  src={img} alt=""/>
                 </div>
 
                 <div>
 
                     <div className="innerDiv">
-                        <h2>Oferta praca java fullstack bohater</h2>
+                        <div>
+                            <h2>Oferta praca java fullstack bohater</h2>
+                        </div>
 
-                        <select>
-                            <option>3 Localizacji </option>
-                            <option value="1">Poznań</option>
-                            <option value="2">Warszawa</option>
-                            <option value="3">Wrocław</option>
-                        </select>
+                         <div className="customSelect"><GrLocation/>
+                            <select>
+                                <option>3 Localizacji </option>
+                                <option value="1">Poznań</option>
+                                <option value="2">Warszawa</option>
+                                <option value="3">Wrocław</option>
+                            </select>
 
-                        <h4>Wymagania:</h4>
-                        javajavajavajava
+                        </div>
+
+                        <div>
+                            <h4>Wymagania:</h4>
+                            <p>Java, CSS, HTML5</p>
+                        </div>
+
                     </div>
+
+
+
+                </div>
+                <div>
+                    <h3><GiGraduateCap/> Junior</h3>
+
+                </div>
+
+            </Card>
+            <Card className="nested">
+
+                <div>
+                    <img className="com-logo"  src={logo} alt=""/>
+                </div>
+
+                <div>
+
+                    <div className="innerDiv">
+                        <div>
+                            <h2>Oferta praca java fullstack bohater</h2>
+                        </div>
+
+                         <div className="customSelect"><GrLocation/>
+                            <select>
+
+                                <option>3 Localizacji </option>
+                                <option value="1">Poznań</option>
+                                <option value="2">Warszawa</option>
+                                <option value="3">Wrocław</option>
+                            </select>
+
+                        </div>
+
+                        {/*<div>*/}
+                        {/*    <h4>Wymagania:</h4>*/}
+
+                        {/*</div>*/}
+                        <div className="job-req">
+                            <p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p>
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <div>
+                    <h3><GiGraduateCap/> Junior</h3>
 
                 </div>
 
