26acdb6fd7b3af0c187c0ec057568c5e7939e522
JakubGem <jakegemb89@gmail.com>
2022-11-26 11:50:50 +0100

    offer card: all sections in beta version are ready

--- src/main/front/src/components/offers-page/Offers.jsx
+++ src/main/front/src/components/offers-page/Offers.jsx
@@ -7,6 +7,9 @@ import "./offers.css";
 import {GrLocation} from "react-icons/gr";
 import {GiGraduateCap} from "react-icons/gi";
 import {AiTwotoneExperiment} from "react-icons/ai";
+import {RiMoneyDollarCircleFill} from "react-icons/ri";
+import {IoStar, IoStarOutline} from "react-icons/io5";
+import {HiDocumentText} from "react-icons/hi";
 const Offers = () => {
     return (
         <div>
@@ -94,7 +97,7 @@ const Offers = () => {
 
                     <div className="innerDiv">
                         <div>
-                            <h2>Oferta praca java fullstack bohater</h2>
+                            <h2> <a style={{textDecoration: "none", color: "black"}} href="#oferta"> Oferta praca java fullstack bohater </a> </h2>
                         </div>
 
                          <div className="customSelect"><GrLocation/>
@@ -135,21 +138,21 @@ const Offers = () => {
 
                     <div className="innerDiv">
                         <div>
-                            <h2>Oferta praca java fullstack bohater</h2>
+                            <h2><a style={{textDecoration: "none", color: "black"}} href="#oferta"> Oferta praca java fullstack bohater </a></h2>
+                            <h6>MC Don sp. zo.o. ★★★★☆</h6>
                         </div>
 
-                         <div className="customSelect"><GrLocation/>
+                        <div className="customSelect"><GrLocation/>
                             <select>
-
                                 <option>3 Localizacji </option>
-                                <option value="1">Poznań</option>
+                                <option value="1">Poznań, Wielkopolskie</option>
                                 <option value="2">Warszawa</option>
                                 <option value="3">Wrocław</option>
                             </select>
 
 
 
-                                 <span> <AiTwotoneExperiment/>  Doświadczenie: min 3 lata na podobnym stanowisku</span>
+                            <span> <AiTwotoneExperiment/>  Doświadczenie: min 3 lata na podobnym stanowisku</span>
 
 
 
@@ -182,8 +185,76 @@ const Offers = () => {
                     </div>
 
                 </div>
+                <div className="offer-section">
+                    <h3 style={{  marginLeft: "90%"}}><IoStarOutline/> </h3>
+                    <h4><GiGraduateCap/> Junior</h4>
+                    <h4> <RiMoneyDollarCircleFill/> 26 000-32 000 zł netto (+ VAT) / mies.</h4>
+                    <h4> <HiDocumentText/> umowa o pracę, kontrakt B2B</h4>
+                </div>
+
+
+            </Card>            <Card className="nested">
+
                 <div>
-                    <h3><GiGraduateCap/> Junior</h3>
+                    <img className="com-logo"  src={logo} alt=""/>
+                </div>
+
+                <div>
+
+                    <div className="innerDiv">
+                        <div>
+                            <h2><a style={{textDecoration: "none", color: "black"}} href="#oferta"> Oferta praca java fullstack bohater </a></h2>
+                            <h6>MC Don sp. zo.o. ★★★★☆</h6>
+                        </div>
+
+                        <div className="customSelect"><GrLocation/>
+                            <select>
+                                <option>3 Localizacji </option>
+                                <option value="1">Poznań, Wielkopolskie</option>
+                                <option value="2">Warszawa</option>
+                                <option value="3">Wrocław</option>
+                            </select>
+
+
+
+                            <span> <AiTwotoneExperiment/>  Doświadczenie: min 3 lata na podobnym stanowisku</span>
+
+
+
+                        </div>
+
+                        {/*<div>*/}
+                        {/*    <h4>Wymagania:</h4>*/}
+
+                        {/*</div>*/}
+                        <div className="job-req">
+                            <p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p>  <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>HTML5</p><p>Java</p>
+                            <p>CSS</p>
+                            <p>kubernetes/jenkins</p>
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <div className="offer-section">
+                    <h3 style={{  marginLeft: "90%"}}><IoStar/> </h3>
+                    <h4><GiGraduateCap/> Junior</h4>
+                    <h4> <RiMoneyDollarCircleFill/> 26 000-32 000 zł netto (+ VAT) / mies.</h4>
+                    <h4> <HiDocumentText/> umowa o pracę, kontrakt B2B</h4>
                 </div>
 
 
