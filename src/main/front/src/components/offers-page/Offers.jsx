4309b0fc03c795d0f1077bd81246f5a3b924b875
JakubGem <jakegemb89@gmail.com>
2022-11-26 12:45:33 +0100

    offer card: code, css and view refactor

--- src/main/front/src/components/offers-page/Offers.jsx
+++ src/main/front/src/components/offers-page/Offers.jsx
@@ -27,37 +27,30 @@ const Offers = () => {
                         </div>
                         <div className="custom-select"><GrLocation/>
                             <select>
-                                <option>3 Localizacji </option>
+                                <option>7 Localizacji </option>
                                 <option value="1">Poznań, Wielkopolskie</option>
+                                <option value="2">Warszawa</option>
+                                <option value="3">Wrocław</option>
                                 <option value="2">Warszawa</option>
                                 <option value="3">Wrocław</option>
                             </select>
                             <span><AiTwotoneExperiment/> Experience: min. 3 years at similar position</span>
                         </div>
                         <div className="job-req">
-                            <p>Java</p>
+                            <p>C++</p>
                             <p>CSS</p>
                             <p>HTML5</p><p>Java</p>
-                            <p>CSS</p>
-                            <p>HTML5</p><p>Java</p>
-                            <p>CSS</p>
-                            <p>HTML5</p><p>Java</p>
-                            <p>CSS</p>
-                            <p>HTML5</p>  <p>CSS</p>
-                            <p>HTML5</p><p>Java</p>
-                            <p>CSS</p>
-                            <p>HTML5</p><p>Java</p>
-                            <p>CSS</p>
-                            <p>HTML5</p><p>Java</p>
-                            <p>CSS</p>
+                            <p>C#</p>
+                            <p>Grec philosophy</p>
+                            <p>Knowledge of botanic</p>
                             <p>kubernetes/jenkins</p>
                         </div>
                     </div>
                 </div>
                 <div className="offer-section">
                     <h3 style={{  marginLeft: "90%"}}><IoStarOutline/> </h3>
-                    <h4><GiGraduateCap/> Junior</h4>
-                    <h4> <RiMoneyDollarCircleFill/> 26 000-32 000 PLN net (+ TAX) / month.</h4>
+                    <h4><GiGraduateCap/>Mid / Junior / Trainee</h4>
+                    <h4> <RiMoneyDollarCircleFill/> 20 000 / month.</h4>
                     <h4> <HiDocumentText/> Contract of employment, Contract B2B</h4>
                 </div>
             </Card>
@@ -73,12 +66,12 @@ const Offers = () => {
                         </div>
                         <div className="custom-select"><GrLocation/>
                             <select>
-                                <option> 3 Localizacji </option>
+                                <option> 12 Localizacji </option>
                                 <option value="1">Poznań, Wielkopolskie</option>
                                 <option value="2">Warszawa</option>
                                 <option value="3">Wrocław</option>
                             </select>
-                            <span><AiTwotoneExperiment/>  Experience: min. 3 years at similar position</span>
+                            <span><AiTwotoneExperiment/>  Experience: min. 7 years in Java 8+</span>
                         </div>
                         <div className="job-req">
                             <p>Java</p>
@@ -118,12 +111,12 @@ const Offers = () => {
                         </div>
                         <div className="custom-select"><GrLocation/>
                             <select>
-                                <option>3 Localizacji </option>
+                                <option>32 Localizacji </option>
                                 <option value="1">Poznań, Wielkopolskie</option>
                                 <option value="2">Warszawa</option>
                                 <option value="3">Wrocław</option>
                             </select>
-                            <span><AiTwotoneExperiment/> Experience: min. 3 years at similar position</span>
+                            <span><AiTwotoneExperiment/> Experience: N/A</span>
                         </div>
                         <div className="job-req">
                             <p>Java</p>
@@ -162,7 +155,7 @@ const Offers = () => {
                                 <option value="2">Warszawa</option>
                                 <option value="3">Wrocław</option>
                             </select>
-                            <span><AiTwotoneExperiment/>  Experience: min. 3 years at similar position</span>
+                            <span><AiTwotoneExperiment/>  Experience: min. 1 year at similar position</span>
                         </div>
                         <div className="job-req">
 
@@ -175,7 +168,7 @@ const Offers = () => {
                 <div className="offer-section">
                     <h3 style={{  marginLeft: "90%"}}><IoStar/> </h3>
                     <h4><GiGraduateCap/> Junior</h4>
-                    <h4> <RiMoneyDollarCircleFill/> 26 000-32 000 PLN net (+ TAX) / month.</h4>
+                    <h4> <RiMoneyDollarCircleFill/> 9 000-12 000 net/month.</h4>
                     <h4> <HiDocumentText/> Contract of employment</h4>
                 </div>
             </Card>
