1db90b629471e7aa8ded16c3525685d1dd4ae3e5
JakubGem <jakegemb89@gmail.com>
2022-11-28 21:22:54 +0100

    I've created new component SearchPositionBar.jsx, it is implemented and works fine but need some css

--- src/main/front/src/components/offers-page/userRequirements/UserRequirements.jsx
+++ src/main/front/src/components/offers-page/userRequirements/UserRequirements.jsx
@@ -1,20 +1,24 @@
 import React, {useState} from 'react';
 import SearchCityBar from "./searchCityBar/SearchCityBar";
 import {Card} from "react-bootstrap";
+import SearchPositionBar from "./searchPositionBar/SearchPositionBar";
 
 const UserRequirements = () => {
     const [rangeValue, setRangeValue] = useState();
     const handleRangeValue = (e) =>{
         setRangeValue(e.target.value);
     }
+
+
     return (
         <div>
             <Card>
                 <SearchCityBar placeholder="Enter city"/>
+                <SearchPositionBar placeholder="Enter position"/>
                 <div>
                     <h4>Salary</h4>
-                    <input onChange={handleRangeValue} className="custom-slider" type="range" min="1000" max="100000" step="1000"/>
-                    {rangeValue > 0 ? "1000 - " + rangeValue : rangeValue}
+                    <input onChange={handleRangeValue} className="custom-slider" type="range" min="0" max="100000" step="1000"/>
+                    {rangeValue > 1000 ? "1000 - " + rangeValue : rangeValue}
                 </div>
             </Card>
 
---
+++ src/main/front/src/components/offers-page/userRequirements/searchPositionBar/SearchPositionBar.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {Card} from "react-bootstrap";
+import {FaSearchLocation} from "react-icons/fa";
+import CloseButton from "react-bootstrap/CloseButton";
+import {useState} from 'react';
+
+const SearchPositionBar = (props) => {
+    const [selectedPosition, setSelectedPosition] = useState();
+    const [filteredData, setFilteredData] = useState([]);
+    const [enteredWord, setEnteredWord] = useState("");
+    const [currentWord, setCurrentWord] = useState("");
+    let text = '{"employees":[' +
+        '" Backend", ' +
+        '" Analyst", ' +
+        '" DevOps", ' +
+        '" Frontend", ' +
+        '" Fullstack", ' +
+        '" Project Manager", ' +
+        '" QA", ' +
+        '" Tester", ' +
+        '" Mobile", ' +
+        '" Architect", ' +
+        '" Support", ' +
+        '" Tech Lead", ' +
+        '" Embedded", ' +
+        '" Scrum Master", ' +
+        '" Security", ' +
+        '" UI/UX Designer", ' +
+        '" GameDev", ' +
+        '" Data Science/Engineering" ]}';
+
+    const obj = JSON.parse(text);
+    const list = obj.employees;
+    const handleFilter = (event) => {
+        const searchPosition = event.target.value;
+        setCurrentWord(searchPosition);
+        setEnteredWord(searchPosition);
+        const newFilter = list.filter((value) => {
+            return value.toLowerCase().includes(searchPosition.toLowerCase());
+        });
+        newFilter.sort();
+        if(searchPosition === ""){
+            setFilteredData([]);
+        }else{
+            setFilteredData(newFilter);
+        }
+    };
+
+    const handleChosen = (event) => {
+        setSelectedPosition(event.target.value);
+        setFilteredData([]);
+        setEnteredWord("");
+    }
+
+    const clearInput = () =>{
+        setFilteredData([]);
+        setEnteredWord("");
+        setCurrentWord("");
+    }
+
+    return (
+        <div>
+            <Card>
+                <div className="search">
+                    <div className="search-input">
+                        <input type="text"
+                               value={enteredWord}
+                               placeholder={props.placeholder}
+                               onChange={handleFilter}/>
+                        <div className="search-icon">
+                            {currentWord.length < 1 ? (
+                                <FaSearchLocation/>
+                            ) : (
+                                <CloseButton id="clearBtn" onClick={clearInput} />
+                            )}
+                        </div>
+                    </div>
+                    {filteredData.length !==0 && (
+                        <div>
+                            <p>
+                                {filteredData.slice(0,5).reverse().map(items =>
+                                    <option className="data-results" onClick={handleChosen} key={items}>{items}</option>
+                                )}
+                            </p>
+                        </div>
+                    )}
+                    {selectedPosition}
+                </div>
+            </Card>
+        </div>
+    );
+};
+
+export default SearchPositionBar;
